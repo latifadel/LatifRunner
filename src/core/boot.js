451b490c2f86096c44e8fa15e09a06591dd1f4eb
L17 <a.alabdullatif.17@gmail.com>
2025-02-20 23:54:20 -0800

    Update boot.js

--- src/core/boot.js
+++ src/core/boot.js
@@ -1,1 +1,15 @@
+class BootScene extends Phaser.Scene {
+    constructor() {
+        super({ key: "BootScene" });
+    }
 
+    preload() {
+        // Can load a minimal loading bar here
+    }
+
+    create() {
+        this.scene.start("PreloadScene");
+    }
+}
+
+export default BootScene;
