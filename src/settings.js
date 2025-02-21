1b5950065a4b5e6d0e9c1c7a15b82c9318112a17
L17 <a.alabdullatif.17@gmail.com>
2025-02-20 23:48:07 -0800

    Create settings.js

---
+++ src/settings.js
@@ -0,0 +1,10 @@
+export const GAME_SETTINGS = {
+    width: 800,                // Game width
+    height: 600,               // Game height
+    backgroundColor: "#87CEEB", // Sky blue background
+    gravity: 1000,             // Gravity for physics
+    playerSpeed: 300,          // Default player movement speed
+    jumpVelocity: -600,        // Jump force
+    maxObstacles: 5,           // Max obstacles on screen
+    enableDebug: false         // Debug mode
+};
