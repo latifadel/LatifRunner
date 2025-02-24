5429cc422534e067c6de5c456abe84a9a6d85dea
L17 <a.alabdullatif.17@gmail.com>
2025-02-24 06:28:42 -0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+// service-worker.js (minimal)
+const CACHE_NAME = 'latif-runner-shapes-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './main.js',
+  'https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
